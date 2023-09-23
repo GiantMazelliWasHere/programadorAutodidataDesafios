959a64620df998c2fb434a8c4e4cf57f995e5fdf
Eduardo Mazelli <85818284+GiantMazelliWasHere@users.noreply.github.com>
2023-09-23 16:45:04 -0300

    Add files via upload

---
+++ hangman/hangman/forca.py
@@ -0,0 +1,28 @@
+def hangman(word):
+    wrong_guesses = 0
+    stages = ["", "________      ", "|      |      ", "|      0      ", "|     /|\     ", "|     / \     ", "|"]
+    remaining_letters = list(word)
+    letter_board = ["__"] * len(word)
+    win = False
+    print('Welcome to Hangman')
+    while wrong_guesses < len(stages) - 1:
+        print('\n')
+        guess = input("Guess a letter")
+        if guess in remaining_letters:
+            character_index = remaining_letters.index(guess)
+            letter_board[character_index] = guess
+            remaining_letters[character_index] = '$'
+        else:
+            wrong_guesses += 1
+        print((' '.join(letter_board)))
+        print('\n'.join(stages[0: wrong_guesses + 1]))
+        if '__' not in letter_board:
+            print('You win! The word was:')
+            print(' '.join(letter_board))
+            win = True
+            break
+    if not win:
+        print('\n'.join(stages[0: wrong_guesses]))
+        print('You lose! The words was {}'.format(word))
+
+hangman("cat")
